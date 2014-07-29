2a2bf1a5dada64b0ac22035d64f18e6c6154bef1
Neena <junkemail273@gmail.com>
2014-07-29 17:16:24 +0100

    Seeds stuff

--- unilo/db/seeds.rb
+++ unilo/db/seeds.rb
@@ -1,10 +1,15 @@
+require 'open-uri'
+
 f = File.open("#{Rails.root}/public/data.xml")
 @xml = Nokogiri::XML(f)
 f.close
 
 puts "Loaded and parsed XML"
 
-University.delete_all
+
+def find_details(pubukprn, kisid, mode)
+  data = JSON.parse(open("https://data.unistats.ac.uk/api/v2/KIS/Institution/#{pubukprn}/Course/#{kisid}/#{mode.to_i - 1}.json", :http_basic_authentication=>["8YDECS8XPZ9R5RTBA72Z", "password"]).read)
+end
 
 @locations = @xml.css("LOCATION").inject({}) do |hash, loc|
   hash[loc.css("UKPRN").children[0].to_s] = {latitude: loc.css("LATITUDE").children[0].to_s.to_f,
@@ -18,18 +23,22 @@ University.delete_all
 end
 
 @xml.css("INSTITUTION").each do |uni|
-  loc = @locations[uni.css("UKPRN").children[0].to_s] 
-  u = University.create({ukprn: uni.css("PUBUKPRN").children[0].to_s}.merge(loc || {}))
-  uni.css("KISCOURSE").each do |course|
-    c = Course.create(university: u, 
-                      url: course.css("CRSEURL").children[0].to_s,
-                      title: course.css("TITLE").children[0].to_s,
-                      year_abroad?: course.css("YEARABROAD").children[0].to_s,
-                      kisid: course.css("KISCOURSEID").children[0].to_s,
-                      fee: course.css("ENGFEE").children[0].to_s.to_i,
-                      satisfaction: (course.css("NSS Q22").children[0].to_s.to_i)/100.0,
-                      salary_6m: course.css("SALARY INSTMED").children[0].to_s.to_i,
-                      jacs: course.css("JACS").children[0].to_s)
+  pubukprn = uni.css("PUBUKPRN").children[0].to_s
+  unless University.find_by_ukprn(pubukprn)
+    loc = @locations[uni.css("UKPRN").children[0].to_s]
+    u = University.create({ukprn: pubukprn}.merge(loc || {}))
+    uni.css("KISCOURSE").each do |course|
+      course_data = find_details(pubukprn, course.css("KISCOURSEID").children[0].to_s, course.css("KISMODE").children[0].to_s)
+      c = Course.create(university: u, 
+                        url: course.css("CRSEURL").children[0].to_s,
+                        title: course.css("TITLE").children[0].to_s,
+                        year_abroad?: course.css("YEARABROAD").children[0].to_s,
+                        kisid: course.css("KISCOURSEID").children[0].to_s,
+                        fee: course.css("ENGFEE").children[0].to_s.to_i,
+                        satisfaction: (course.css("NSS Q22").children[0].to_s.to_i)/100.0,
+                        salary_6m: course.css("SALARY INSTMED").children[0].to_s.to_i,
+                        jacs: course_data["JACSCodes"][0])
+    end
   end
 end
 
