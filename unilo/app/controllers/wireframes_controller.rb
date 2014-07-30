d194069cd6c7bd2f8e90c7cdf550061b87e08eba
Mayank <mayank@guptamayank.co.uk>
2014-07-30 09:23:11 +0100

    Commit to master.
    All current changes being put into master.

---
+++ unilo/app/controllers/wireframes_controller.rb
@@ -0,0 +1,10 @@
+class WireframesController < ApplicationController
+  def show
+    if params[:path].present?
+      @wireframe_path = "wireframe_#{params[:path]}"
+      render :template => "wireframes/#{params[:path]}"
+    else
+      render :action => 'index'
+    end
+  end
+end
