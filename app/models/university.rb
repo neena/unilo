c1c24837c9f6889fed9a94425e60a71a5b5b925e
Neena <junkemail273@gmail.com>
2014-08-02 00:47:02 +0100

    Let's work now please

--- app/models/university.rb
+++ app/models/university.rb
@@ -6,8 +6,13 @@ class University < ActiveRecord::Base
 
   def set_image_url
     puts "https://api.datamarket.azure.com/Bing/Search/v1/Image?$format=json&Query=%27#{CGI.escape(self.title + " campus")}%27"
-    data = JSON.parse(open("https://api.datamarket.azure.com/Bing/Search/v1/Image?$format=json&Query=%27#{CGI.escape(self.title.gsub('&', '\&') + " campus")}%27", :http_basic_authentication=>["8g4bf1KG8MEmBTM1JMnPv47TbemHVjJrhLfG6ZnOFcU", "8g4bf1KG8MEmBTM1JMnPv47TbemHVjJrhLfG6ZnOFcU"]).read)
-    self.image_url = data["d"]["results"][0]["MediaUrl"]
+    data = JSON.parse(open("https://api.datamarket.azure.com/Bing/Search/v1/Image?$format=json&Query=%27#{CGI.escape(self.title + " campus")}%27", :http_basic_authentication=>["8g4bf1KG8MEmBTM1JMnPv47TbemHVjJrhLfG6ZnOFcU", "8g4bf1KG8MEmBTM1JMnPv47TbemHVjJrhLfG6ZnOFcU"]).read)
+    url = data["d"]["results"][0]["MediaUrl"]
+    x = 1
+    while url.length >= 255
+      url = data["d"]["results"][x]["MediaUrl"]
+      x += 1
+    end
     self.save
   end
 end
